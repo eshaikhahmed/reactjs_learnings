b602a8c5ffeb6385e0ae1b4debdadd3d30ac38a9
Ejaz Shaikh <knowtechnical@gmail.com>
2021-12-29 09:48:17 +0530

    Added

---
+++ rust_learnings/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ListRendering from './components/list-renderings/ListRendering';
+
+function App() {
+  return (
+    <div>
+        <ListRendering />
+    </div>
+  );
+}
+
+export default App;
